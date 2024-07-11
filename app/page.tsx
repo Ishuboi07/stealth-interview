b72ff77ce094d1c73c4e46f4b3ac1c5a6247bb36
Ishaan Agarwal <ishaan.agarwal.cer21@iitbhu.ac.in>
2024-07-11 18:16:35 +0530

    refactor: Update page layout and styles for better user experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-evenly h-screen gap-y-6 ">
-      <div className="h-[90vh] w-[390px] bg-background border border-black overflow-x-hidden">
+      <div className="h-[100%] w-[390px] bg-background overflow-x-hidden">
         <div className="h-[56px] bg-white flex  items-stretch flex-row">
           <div className="flex items-center">
             <Image
@@ -15,7 +15,7 @@ export default function Home() {
             />
           </div>
 
-          <div className="flex flex-col justify-evenly items-center ">
+          <div className="flex flex-col justify-evenly items-center w-full">
             <h1 className="text-[14px] font-extrabold text-text">Checkout</h1>
             <h2 className="text-gray text-[10px] uppercase font-bold">
               popeyes | jp nagar
@@ -28,12 +28,14 @@ export default function Home() {
             name: "Caribbean Spicy Zinger Burger",
             calories: "350 Kcal",
             price: "₹ 319",
+            items: 1,
           },
           {
             image: "/assets/burger2.png",
             name: "Canjun Veg Burger",
             calories: "250 Kcal",
             price: "₹ 219",
+            items: 2,
           },
         ].map((item, index) => (
           <div
@@ -60,7 +62,11 @@ export default function Home() {
               </div>
               <div className="flex flex-row justify-between">
                 <p className="font-extrabold">{item.price}</p>
-                <p>some ui design</p>
+                <div className="w-[88px] h-[28px] bg-primaryBackground border border-primary rounded-md flex flex-row justify-between text-primary px-[10px] font-bold">
+                  <p>-</p>
+                  <p>{item.items}</p>
+                  <p>+</p>
+                </div>
               </div>
             </div>
           </div>
@@ -69,7 +75,31 @@ export default function Home() {
           <p className="text-text font-extrabold ml-[10px]">
             Frequently Ordered
           </p>
-          carousel
+          <div className="flex flex-row my-4 w-[60vw]">
+            {[1, 2, 3].map((item, index) => (
+              <div
+                className="rounded-card bg-white w-[148px] h-[140px] p-2 ml-[10px]"
+                key={index}
+              >
+                <div className="w-full h-[80px] rounded-card">
+                  <Image
+                    src="/assets/Fanta.png"
+                    alt="fanta"
+                    width={100}
+                    height={100}
+                    className="object-cover h-full w-full rounded-card"
+                  />
+                </div>
+                <div className="flex flex-col  justify-between h-[30%] mt-3">
+                  <p className="text-[10px] font-extrabold text-text">
+                    Fanta - 1 PET
+                  </p>
+
+                  <p className="text-[10px] font-extrabold">₹ 89</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         <div className="mx-[14px] bg-white h-[146px] rounded-card flex flex-col  justify-around p-[14px]">
           <div className="flex flex-row gap-4">
@@ -89,6 +119,7 @@ export default function Home() {
               </p>
             </div>
           </div>
+          <div className="w-full bg-black h-[1px]"></div>
           <div className="flex flex-row gap-4">
             <div className="aspect-square w-8 ">
               <Image
@@ -112,7 +143,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <button className="h-[44px] rounded-card bg-primary text-white w-[90%] text-[16px] mx-[14px] mt-10 font-bold">
+        <button className="h-[44px] rounded-card bg-primary text-white w-[93%] text-[16px] mx-[14px] mt-2 font-bold">
           Proceed to checkout
         </button>
       </div>
